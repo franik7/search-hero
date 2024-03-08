9dd61859268f1a83c1d9f5a7af53a8984aa54772
Frants <118231497+franik7@users.noreply.github.com>
2024-03-08 13:13:45 -0500

    Add files via upload

--- public/main.js
+++ public/main.js
@@ -4,9 +4,10 @@ const randomHelper = document.querySelector('#random-helper')
 const viewAll = document.querySelector('#view-all')
 const sortByNameButton = document.querySelector("#sort-by-name")
 const sortByDateButton = document.querySelector("#sort-by-date")
+const sortByLikesButton = document.querySelector("#sort-by-likes")
 
-// const baseURL = `http://localhost:5678/api/products`
-const baseURL = `https://productfinder-5frz.onrender.com/api/products`
+const baseURL = `http://localhost:5678/api/products`
+// const baseURL = `https://product-finder-e2ej.onrender.com/api/products`
 
 const productsCallback = products => {
   console.log("Products received in frontend:", products)
@@ -39,16 +40,22 @@ const deleteProduct = id => axios.delete(`${baseURL}/${id}`)
 .catch(errCallback)
 
 
+const updateLike = (id, likes) => axios.put(`${baseURL}/${id}`, {likes})
+
+.then(() => {
+  console.log(`Product with id:${id} updated`)
+  productsCallback
+  getAllProducts()
+})
+.catch(errCallback)
+
+
 const emailAboutProduct = id => axios.get(`${baseURL}/${id}`)
 .then(response => {
   console.log(response.data[0].email)
   console.log(response.data[0].product_name)
-  let subject = `I found your ${response.data[0].product_name} product on ProductHero!`
-  let body = `Dear ProductHero user, your product ${response.data[0].product_name} was found on ProductHero!
-  Here it is: (please list the URL or store where the product was located)
-  
-  Thank you.
-  Your ProductHero! 
+  let subject = `I found your ${response.data[0].product_name} product on SearchHero!`
+  let body = ` (please list the URL or store where the product was located)
   `
   let composedEmail = "mailto:" + response.data[0].email + "?subject=" + subject + "&body=" + body;
   window.location.href = composedEmail;
@@ -89,20 +96,39 @@ function createProductCard(product) {
   <div class="card-bootstrap p-1">
     <div class="col">
       <div class="card" style="width: 18rem;">
+        <!-- Product Image -->
         <img src="${product.url}" class="card-img-top" alt="product">
+        
         <div class="card-body">
+          <!-- Product Title -->
           <h5 class="card-title">${product.product_name}</h5>
+          
           <div class="d-flex justify-content-around">
             <button onclick="emailAboutProduct(${product.id})" class="btn btn-primary">Email</button>
             <button onclick="deleteProduct(${product.id})" class="btn btn-danger">Delete</button>
           </div>
         </div>
+        
+        <div class="d-flex justify-content-center">
+          <p>I am also looking for this product</p>
+        </div>
+  
+        <div class="d-flex justify-content-center"> 
+        <button onclick="updateLike(${product.id})" class="btn btn-danger">
+          <!-- Custom Thumbs-Up Image -->
+          <img class="color-change" src="./assets/hand-thumbs-up.svg" alt="thumbs-up">
+          
+          <!-- Like Counter -->
+          <p class="ml-2 mb-0">${product.likes}</p>
+        </button>
+      </div>
+      
+        
       </div>
     </div>
   </div>
-
-
 `;
+
 
 
   productsContainer.appendChild(productCard)
@@ -151,14 +177,20 @@ function displayProducts(arr) {
     productsCallback(response.data)
   })
   .catch(errCallback)
+
+  const sortByLikes = () => axios.get(`${baseURL}/likes`).then(response => {
+    productsCallback(response.data)
+  })
+  .catch(errCallback)
+
   
-
-
   sortByDateButton.addEventListener('click', sortByDate)
   sortByNameButton.addEventListener('click', sortByName)
+  sortByLikesButton.addEventListener('click', sortByLikes)
   form.addEventListener('submit', submitHandler)
   randomHelper.addEventListener('click', displayRandomProduct)
   viewAll.addEventListener('click', getAllProducts)
+
   
   getAllProducts()
 
